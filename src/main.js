7e4eff742b02a6d26bbb67c8923f29a86e801a93
sunkaidi <sunkaidi@qq.com>
2022-03-26 21:49:05 +0800

    完成了vant初始化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './style/index.less'
 
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
+// 注册使用vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
